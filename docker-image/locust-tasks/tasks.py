fd1efd417f522e1997b312496271771da516adbe
eklicious <eugenekang@gmail.com>
2021-08-26 13:15:41 -0400

    changed tasks.py to properly use the mdb client

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -8,41 +8,22 @@ import pymongo
 from bson import json_util
 from bson.json_util import loads
 from bson import ObjectId
-from locust import HttpUser, events, task, constant, tag
+from locust import User, events, task, constant, tag
 import time
 
-class MetricsLocust(HttpUser):
-    mdbClient = pymongo.MongoClient("mongodb+srv://ek:ek@demo.nndb3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&readPreference=secondary")
-    coll = mdbClient.sample_airbnb.listingsAndReviews
+mdbClient = pymongo.MongoClient("mongodb+srv://pentaho:pentaho@winvargo.6ngvd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&readPreference=secondary")
+coll = mdbClient.sample_airbnb.listingsAndReviews
 
-    # Start logic per user
-    # def on_start(self):
-    # self._deviceid = str(uuid.uuid4())
-
-    @events.test_start.add_listener
-    def on_test_start(environment, **kwargs):
-        print("A new test is starting")
-
-    @events.test_stop.add_listener
-    def on_test_stop(environment, **kwargs):
-        print("A new test is ending")
-
-    def _async_find(self, timeout=600):
+class MetricsLocust(User):
+    @task(1)
+    def _async_find(self):
         try:
             tic = time.time();
             # Get the record from the TEST collection now
-            doc = self.coll.find_one({})
+            coll.find_one({}, {"_id":1})
             events.request_success.fire(request_type="pymongo", name="singleFetch", response_time=(time.time()-tic), response_length=0)
         except KeyboardInterrupt:
             gevent.sleep(0)
         except Exception as e:
-            print(f'DB-CONNECTION-PROBLEM: '
+            print(f'Exception: '
                   f'{str(e)}')
-            connect_problem = True
-
-    @task(1)
-    def test(self):
-        # If we don't sleep, locust tries to launch as many greenlets as possible which will crash the machine
-        # Ideally, we'll sleep less than the overall avg execution time, e.g. <2ms.
-        gevent.sleep(0.004)
-        gevent.spawn(self._async_find)
