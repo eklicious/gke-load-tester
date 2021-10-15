af82482cb336215a2a73bb9f86bbfa714793cf91
eklicious <eugenekang@gmail.com>
2021-10-15 15:34:12 -0400

    Update tasks.py

--- docker-image/locust-tasks/tasks.py
+++ docker-image/locust-tasks/tasks.py
@@ -12,6 +12,9 @@ from bson import ObjectId
 from locust import User, events, task, constant, tag, between
 import time
 
+########################################################################
+# Global Static Variables that can be accessed without referencing self
+########################################################################
 # Change the connection string to point to a correct db and double check the readpreference etc.
 mdbClient = pymongo.MongoClient("mongodb+srv://<username>:<password>@<srv>/myFirstDatabase?retryWrites=true&w=majority&readPreference=secondaryPreferred")
 coll = mdbClient.sample_airbnb.listingsAndReviews
@@ -20,10 +23,17 @@ class MetricsLocust(User):
     # Set the wait time to be long enough so the Locust user can actually finish any of the operations, e.g. don't make Locust go too fast else it'll start to queue up too many gevent operations and will eventually cause the CPU to hit 100% and crash.
     wait_time = between(1, 1)
 
+    # Helper function that is not a Locust task. All Locust tasks require the @task annotation
+    # Note that you have to pass the self reference for all helper functions
+    def get_time(self):
+        return time.time()
+    
     @task(1)
     def _async_find(self):
         try:
-            tic = time.time();
+            # Note that you don't pass in self despite the signature above
+            tic = get_time();
+            
             # Get the record from the TEST collection now
             coll.find_one({}, {"_id":1})
             events.request_success.fire(request_type="pymongo", name="singleFetch", response_time=(time.time()-tic), response_length=0)
